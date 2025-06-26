5eadb18dd9ece97002416e5694e16a05abe6feea
TASLIM-ENIOLLA <taslim.eniolla@gmail.com>
2025-06-26 13:15:02 +0100

    Tailwind 3 update to 4

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,21 +1,6 @@
 const config = {
   plugins: {
-    tailwindcss: {
-      config: {
-        content: [
-          './pages/**/*.{js,ts,jsx,tsx}',
-          './components/**/*.{js,ts,jsx,tsx}',
-          './app/**/*.{js,ts,jsx,tsx}',
-          './src/**/*.{js,ts,jsx,tsx}',
-        ],
-        theme: {
-          extend: {},
-        },
-        plugins: [],
-      }
-    },
-    autoprefixer: {},
+    "@tailwindcss/postcss": {},
   },
 };
-
 export default config;
